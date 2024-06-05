360f79572c1724428e7e764180669ac5ebc2dadd
Bartek Pacia <barpac02@gmail.com>
2024-06-06 00:40:42 +0100

    GeocacheDescriptionRoute: make passing HTML as query params work (somehow)

--- composeApp/src/commonMain/kotlin/tech/pacia/opencaching/Navigation.kt
+++ composeApp/src/commonMain/kotlin/tech/pacia/opencaching/Navigation.kt
@@ -5,16 +5,17 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import tech.pacia.opencaching.features.geocache.GeocacheRoute
 import tech.pacia.opencaching.features.geocache.description.GeocacheDescriptionRoute
 import tech.pacia.opencaching.features.map.MapRoute
 import tech.pacia.opencaching.features.signin.SignInRoute
 
 private object Destinations {
-    const val SIGN_IN_ROUTE = "signin"
-    const val MAP_ROUTE = "map/"
-    const val GEOCACHE_ROUTE = "geocache/{code}"
-    const val GEOCACHE_DESCRIPTION_ROUTE = "geocache/{code}/description"
+    // const val SIGN_IN_ROUTE = "signin/"
+    // const val MAP_ROUTE = "map/"
+    // const val GEOCACHE_ROUTE = "geocache/{code}"
+    // const val GEOCACHE_DESCRIPTION_ROUTE = "geocache/{code}/description"
 }
 
 @Composable
@@ -23,15 +24,15 @@ fun OpencachingNavHost(
 ) {
     NavHost(
         navController = navController,
-        startDestination = Destinations.SIGN_IN_ROUTE,
+        startDestination = "signin/",
     ) {
-        composable(Destinations.SIGN_IN_ROUTE) {
+        composable("signin/") {
             SignInRoute(
-                onNavigateToMap = { navController.navigate(Destinations.MAP_ROUTE) },
+                onNavigateToMap = { navController.navigate("map/") },
             )
         }
 
-        composable(Destinations.MAP_ROUTE) {
+        composable("map/") {
             MapRoute(
                 onNavigateToGeocache = { cache ->
                     navController.navigate("geocache/${cache.code}")
@@ -39,7 +40,7 @@ fun OpencachingNavHost(
             )
         }
 
-        composable(Destinations.GEOCACHE_ROUTE) { backstackEntry ->
+        composable("geocache/{code}") { backstackEntry ->
             val code = backstackEntry.arguments?.getString("code") ?: "empty"
             GeocacheRoute(
                 code = code,
@@ -50,7 +51,10 @@ fun OpencachingNavHost(
             )
         }
 
-        composable(Destinations.GEOCACHE_DESCRIPTION_ROUTE) { backstackEntry ->
+        composable(
+            route = "geocache/{code}/description?html={html}",
+            arguments = listOf(navArgument("html") { defaultValue = "defaultValue" })
+           ) { backstackEntry ->
             val html = backstackEntry.arguments?.getString("html") ?: "empty"
             GeocacheDescriptionRoute(
                 html = html,
