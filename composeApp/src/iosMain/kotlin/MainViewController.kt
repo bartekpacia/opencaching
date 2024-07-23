f774a8d71ba738bfdec9975452cf9b11a717ca7f
Bartek Pacia <barpac02@gmail.com>
2024-07-23 14:13:12 +0200

    add an example Maestro flow

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,10 +1,14 @@
+import androidx.compose.runtime.ExperimentalComposeApi
+import androidx.compose.ui.platform.AccessibilitySyncOptions
 import androidx.compose.ui.window.ComposeUIViewController
 import platform.UIKit.UIViewController
 import tech.pacia.opencaching.App
 
+@OptIn(ExperimentalComposeApi::class)
 @Suppress("FunctionNaming")
 fun MainViewController(): UIViewController {
-    return ComposeUIViewController {
-        App()
-    }
+    return ComposeUIViewController(
+        configure = { accessibilitySyncOptions = AccessibilitySyncOptions.Always(null) },
+        content = { App() }
+    )
 }
