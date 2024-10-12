b7b2fc24251b9b1d4fffb78d5fda6d303b7cc84a
Bartek Pacia <barpac02@gmail.com>
2024-10-12 02:27:07 +0200

    Remove unused imports from MapScreen.kt

--- composeApp/src/commonMain/kotlin/tech/pacia/opencaching/features/map/MapScreen.kt
+++ composeApp/src/commonMain/kotlin/tech/pacia/opencaching/features/map/MapScreen.kt
@@ -1,10 +1,5 @@
 package tech.pacia.opencaching.features.map
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateMapOf
@@ -27,7 +22,6 @@ import tech.pacia.opencaching.data.CachesRepository
 import tech.pacia.opencaching.debugLog
 import kotlin.time.Duration.Companion.seconds
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MapScreen(
     onNavigateToGeocache: (Geocache) -> Unit,
